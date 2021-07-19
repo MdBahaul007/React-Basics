1ad2ec2fa3d876c49d5a86a935679a6d2009c1b4
TeamIonic00 <mdbahaul007gmail.com>
2021-07-20 01:57:11 +0530

    2nd

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './Componenets/counter';
-import Counters from './Componenets/counters';
+import reactDom from 'react-dom';
+
 // import App from './App';
 
 
+// ReactDOM.render(
+//     <div>
+//         <h1>Hello</h1>
+//         <p>How are you</p>
+//     </div>,
+//     document.getElementById("root"));
 
-ReactDOM.render(<Counter />,document.getElementById("root"));
-ReactDOM.render(<Counters />,document.getElementById("root"));
+//above using another method
+
+// ReactDOM.render(
+//     [
+//         <h1>Hello</h1>,
+//         <p>How are you doing.</p> 
+
+//     ],
+// document.getElementById("root"));
+
+
+//above using another method
+// ReactDOM.render(
+//     <React.Fragment>
+//         <h1>Hello</h1>
+//         <p>How are you....</p>
+//     </React.Fragment>,
+//     document.getElementById("root")
+// )
+
+//above using another method
+// ReactDOM.render(
+//     <>
+//      <h1>Hello</h1>
+//      <p>Top 5 Web Series.</p>
+//      <ol>
+//          <li>Games of Thrones </li>
+//          <li>Money Heist</li>
+//          <li>Prison Break</li>
+//          <li>Special Ops</li>
+//          <li>Family Man</li>
+//      </ol>
+//     </>,
+//      document.getElementById("root")
+// );
+
+const name="mbm"
+ReactDOM.render(
+    <React.Fragment>
+        <h1>My name is {name}</h1>
+        <h1>{4*4}</h1>
+    </React.Fragment>,
+    document.getElementById("root")
+);
